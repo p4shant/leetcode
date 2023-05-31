df6eef9239d315ed2bd79b42dd5becbe3329ede9
Prashant Singh <101547414+p4shant@users.noreply.github.com>
2023-05-31 17:26:45 +0530

    Time: 5 ms (37.18%), Space: 6 MB (48.43%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        
-        // finding all pairs of squares
-        vector<long long int> sq;
-        long long int tmp=pow(c,0.5);
-        for(int i=0;i<=tmp;i++)
-            sq.push_back(i*i);
-        
         //searching the pair
-        long long int start=0,end=sq.size()-1;
+        long long int start=0,end=pow(c,0.5);
         while(start<=end){
-            long long int t=sq[start]+sq[end];
+            long long int t=start*start+end*end;
             if(t==c)
                 return true;
             else if(t>c)
