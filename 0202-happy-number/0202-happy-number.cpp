81fecb76fcaa4e7437c39db437299774295a67de
Prashant Singh <101547414+p4shant@users.noreply.github.com>
2023-06-15 21:14:57 +0530

    Time: 6 ms (22.99%), Space: 6.2 MB (44.07%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -3,20 +3,19 @@ public:
     bool isHappy(int n) {
         if(n==1)
         return true;
-        int cnt=2511;
-        while(n!=1&&cnt!=0){
-            long long int sm=0;
+        unordered_map<int,int> mp;
+        while(n!=1){
+            if(mp.find(n)!=mp.end())
+                return false;
+            mp[n]++;
+            long long int tmp=0;
             while(n!=0){
-                sm+=(n%10)*(n%10);
-                n=n/10;
+                tmp+=(n%10)*(n%10);
+                n/=10;
             }
-            if(sm==1)//happy number
-            return true;
-            else if(sm==n)//loop
-            return false;
-            
-            n=sm;
-            cnt--;
+            if(tmp==1)
+                return true;
+            n=tmp;
         }
         return false;
     }
