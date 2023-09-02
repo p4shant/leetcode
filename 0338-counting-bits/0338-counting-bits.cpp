e5a143de462dac2755c6a5b5da22cf8f971d13b1
Prashant Singh <101547414+p4shant@users.noreply.github.com>
2023-09-02 09:36:42 +0530

    Time: 9 ms (25.1%), Space: 9.1 MB (6.05%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
 
-    int count(int i){
+    int count(int i, vector<int> &sex){
 
-        int cnt=0;
+        int cnt=0,n=i;
+
         while(i>0){
 
             ++cnt;
@@ -12,21 +13,30 @@ public:
             int lodu=pow(2,x);
 
             if(lodu==i){
+                sex[n]=cnt;
                 return cnt;
             }
             else{
                 i=i-lodu;
             }
 
+            if(sex[i]!=-1){
+                sex[n]=cnt+sex[i];
+                return cnt+sex[i];
+            }
+
         }
+        sex[n]=cnt;
         return cnt;
     }
 
     vector<int> countBits(int n) {
 
         vector<int> bit;
+        vector<int> sex(n+1,-1);
+
         for(int i=0;i<=n;i++){
-            int b=count(i);
+            int b=count(i,sex);
             bit.push_back(b);
         }
         return bit;
