c3acb89f5809c2013434d16706db32e6d2143fd1
Prashant Singh <101547414+p4shant@users.noreply.github.com>
2023-09-04 12:07:12 +0530

    Time: 8 ms (8.97%), Space: 16.4 MB (21.2%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     
-    void fn( vector<vector<int>> &ans, vector<int> tmp, vector<int> &nums, int start){
+    void fn( vector<vector<int>> &ans, vector<int> tmp, vector<int> nums){
 
-        ans.push_back(tmp);
-
-        for(int i=start;i<nums.size();i++){
-
-            tmp.push_back(nums[i]);
-            fn(ans,tmp,nums,i+1);
-            tmp.pop_back();
-
+        if(nums.size()==0){
+            ans.push_back(tmp);
+            return;   
         }
+
+        int sex=nums[0];
+        nums.erase(nums.begin());
+
+        fn(ans,tmp,nums);
+
+        tmp.push_back(sex);
+
+        fn(ans,tmp,nums);
 
     }
 
@@ -19,7 +23,7 @@ public:
         
         vector<vector<int>> ans;
         vector<int> tmp;
-        fn(ans,tmp,nums,0);
+        fn(ans,tmp,nums);
 
         return ans;
 
