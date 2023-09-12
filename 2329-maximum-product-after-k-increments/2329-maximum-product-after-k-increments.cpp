f05696d6750f4d0a8105a445c455dffd4872dcef
Prashant Singh <101547414+p4shant@users.noreply.github.com>
2023-09-12 22:26:10 +0530

    Time: 441 ms (21.86%), Space: 90.9 MB (28.07%) - LeetHub

--- 2329-maximum-product-after-k-increments/2329-maximum-product-after-k-increments.cpp
+++ 2329-maximum-product-after-k-increments/2329-maximum-product-after-k-increments.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums, int k) {
-        long long int mod=pow(10,9)+7;
+        long long int mod=1e9+7;
         priority_queue <int, vector<int>, greater<int> > pq;
         for(auto &it:nums)
         pq.push(it);
