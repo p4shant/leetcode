c4fe1d8fb6ec0f71e00433749f5ccea94413170e
Prashant Singh <101547414+p4shant@users.noreply.github.com>
2023-09-18 08:31:10 +0530

    Time: 12 ms (37.39%), Space: 11.2 MB (9.15%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
+
+    int fn(vector<int> tmp){
+        int low=0,high=tmp.size()-1;
+        while(low<high){
+            int md=(low+high)/2;
+            if(tmp[md]==1){
+                low=md+1;
+            }
+            else{
+                high=md;
+            }
+        }
+        if(tmp[high]==1)
+        return high+1;
+        else
+        return low;
+    }
+
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         priority_queue < pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
-        int n=mat.size(),m=mat[0].size();
-        for(auto i=0;i<n;i++){
-            int cnt=0;
-            for(auto j=0;j<m;j++){
-                cnt+=mat[i][j];
-            }
+        int n=mat.size(),m=mat[0].size(),i=0;
+        for(auto &it:mat){
+            int cnt=fn(it);
+            cout<<cnt<<" ";
             pq.push(make_pair(cnt,i));
+            ++i;
         }
         vector<int> ans;
         while(k--){
